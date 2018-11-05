7fcb799159be3c751ec9653c508c42c8cfe55d71
cmohub4 test <cmohubtest4@outlook.com>
2018-11-04 18:59:18 -0800

    d

--- FunctionApp1/FunctionApp1/Function1.cs
+++ FunctionApp1/FunctionApp1/Function1.cs
@@ -13,7 +13,7 @@ namespace FunctionApp1
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> Run( 
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
